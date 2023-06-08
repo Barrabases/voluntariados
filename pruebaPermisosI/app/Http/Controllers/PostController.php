2d8fcd80eeb9387e4627bf833849a8175e8d5892
Alan Verdejo <102974028+sdriibs@users.noreply.github.com>
2023-06-08 17:15:04 -0400

    Documentación de los controladores
    Aquí se ven todo tipo de controladores del programa, por ende es una fuente indispensable para que el programa funcione con regularidad

--- pruebaPermisosI/app/Http/Controllers/PostController.php
+++ pruebaPermisosI/app/Http/Controllers/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
         $posts = Post::all();
         return view('posts.index', compact('posts'));
     }
-  
+    //----------------------------------------------------------------------------
     /**
      * Show the form for creating a new resource.
      */
@@ -27,61 +27,58 @@ class PostController extends Controller
         //
         return view('posts.index');
     }
-
+    //----------------------------------------------------------------------------
     /**
-     * Store a newly created resource in storage.
+     * En esta funcion se almacena cualquier tipo de recuerso creado en la base de datos
      */
     public function store(Request $request)
     {
         //return $request->all();
-        $post = new Post;
-        $userId = Auth::id();
-
-        $post->title = $request->title;
-        $post->body = $request->body;
-        $post->created_by = $userId;
-        if ($request->hasFile('image')){
-            $file = $request->file('image');
+        $post = new Post; // Nuevo post.
+        $userId = Auth::id(); // Autentificacion por id.
+        $post->title = $request->title; // Titulo del post.
+        $post->body = $request->body;  // Cuerpo del post.
+        $post->created_by = $userId;  // Organizacion la cual creo el post.
+        if ($request->hasFile('image')){ // En este trozo de codigo se hace el cambio de la imagen
+            $file = $request->file('image'); // para que se almacene en la carpeta 'public' y la reconosca como archivo.
             $path = Storage::putFile('public/images/', $request->file('image'));
             $nuevo_path = str_replace('public/','',$path);
             $post->image_url = $nuevo_path;
         }
-        $post->save();
-        return redirect()->route('posts.index');
+        $post->save(); // se guarda el post
+        return redirect()->route('posts.index'); // y la ruta
         
     }
-
+    //----------------------------------------------------------------------------
     /**
      * Display the specified resource.
      */
     public function show(Post $post)
     {
-        //
+        //Estos recursos no fueron modificados pero sirven para darle mas funcionalidades al programa
     }
-
+     //----------------------------------------------------------------------------
     /**
      * Show the form for editing the specified resource.
      */
     public function edit(Post $post)
     {
-        //
+        //Estos recursos no fueron modificados pero sirven para darle mas funcionalidades al programa
     }
-
+    //----------------------------------------------------------------------------
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, Post $post)
     {
-        //
+        //Estos recursos no fueron modificados pero sirven para darle mas funcionalidades al programa
     }
-
+    //----------------------------------------------------------------------------
     /**
-     * Remove the specified resource from storage.
+     * Funcion para eliminar un post de la pagina y base de datos con solo un boton
      */
     public function destroy($post_id)
     {
-        //Función para destruir las publicaciones.
-
         $post = Post::find($post_id);
         if($post->image_url){
             Storage::delete('public/'.$post->image_url);
